f284e80b33ed6a46dd3aaad8bffe3a2284b9c4a7
Clément Cardonnel <clement.cardonnel@me.com>
2022-11-10 12:23:34 +0100

    Initial Commit

---
+++ MessageH265 Watch App/MessageH265App.swift
@@ -0,0 +1,17 @@
+//
+//  MessageH265App.swift
+//  MessageH265 Watch App
+//
+//  Created by Clément Cardonnel on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MessageH265_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
